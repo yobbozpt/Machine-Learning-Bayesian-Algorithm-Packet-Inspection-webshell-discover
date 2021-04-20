cd84cc0ad85a4fa11b611d31aaa8f142aae6718b
yobbozpt <81952053+yobbozpt@users.noreply.github.com>
2021-04-20 17:20:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@
 import listen_traffic
 
 if __name__ == '__main__':
-    port_name = "VirtualBox Host-Only Ethernet Adapter"
+    port_name = "VirtualBox Host-Only Ethernet Adapter"#写入自己网卡名称
     port_filter = "tcp port 80 8080"
     listen_traffic.ListenTraffic().run(port_name=port_name, port_filter=port_filter)
+
+    
+    #WARNING: Calling str(pkt) on Python 3 makes no sense!（想知道这个是什么问题）
+    #ERROR: Could not compile filter expression tcp port 80 8080（）还有这个问题
